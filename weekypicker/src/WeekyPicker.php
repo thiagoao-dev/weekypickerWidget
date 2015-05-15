73b7c59e59f99c0f059682d913de76483297a2c6
Thiago Augustus <thiagoaugustusdeoliveira@gmail.com>
2015-05-15 16:58:21 -0300

    Empty plugin widget

---
+++ weekypicker/src/WeekyPicker.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 thiagoaugustus! Consulting Group LLC
+ * @link http://startuping.co
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+namespace thiagoaugustus\weekypicker;
+
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\widgets\InputWidget;
+
+/**
+ *
+ * TinyMCE renders a tinyMCE js plugin for WYSIWYG editing.
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @link http://www.ramirezcobos.com/
+ * @link http://www.2amigos.us/
+ */
+class WeekyPicker extends InputWidget
+{
+    /**
+     * @var string the language to use. Defaults to null (en).
+     */
+    public $language;
+    /**
+     * @var array the options for the TinyMCE JS plugin.
+     * Please refer to the TinyMCE JS plugin Web page for possible options.
+     * @see http://www.tinymce.com/wiki.php/Configuration
+     */
+    public $clientOptions = [];
+    /**
+     * @var bool whether to set the on change event for the editor. This is required to be able to validate data.
+     * @see https://github.com/2amigos/yii2-tinymce-widget/issues/7
+     */
+    public $triggerSaveOnBeforeValidateForm = true;
+
+    public $weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+    public $hours    = ['min' => 0, 'max' => 23];
+    public $minutes  = ['min' => 0, 'max' => 59];
+    public $month    = ['min' => 1, 'max' => 31];
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $this->options['class'] = "weekypicker";
+        $this->options['type']  = "hidden";
+        $this->options['value'] = "example";
+
+        $html = <<< 'HTML'
+        <div class="row">
+        <div class="col-lg-12">
+        <div class="btn btn-default">D</div>
+        <div class="btn btn-default">S</div>
+        <div class="btn btn-success">T</div>
+        <div class="btn btn-default">Q</div>
+        <div class="btn btn-default">Q</div>
+        <div class="btn btn-success">S</div>
+        <div class="btn btn-default">S</div>
+        <div class="btn btn-info">Todos</div>
+HTML;
+
+        if ($this->hasModel()) {
+            echo Html::activeTextInput($this->model, $this->attribute, $this->options);
+        } else {
+            echo Html::textInput($this->name, $this->value, $this->options);
+        }
+        $html .= <<< 'HTML'
+</div>
+</div>
+HTML;
+
+        echo $html;
+
+        //$this->registerClientScript();
+    }
+
+    /**
+     * Registers tinyMCE js plugin
+     */
+    protected function registerClientScript()
+    {
+        $js = [];
+        $view = $this->getView();
+
+        TinyMceAsset::register($view);
+
+        $id = $this->options['id'];
+
+        $this->clientOptions['selector'] = "#$id";
+        // @codeCoverageIgnoreStart
+        if ($this->language !== null) {
+            $langFile = "langs/{$this->language}.js";
+            $langAssetBundle = TinyMceLangAsset::register($view);
+            $langAssetBundle->js[] = $langFile;
+            $this->clientOptions['language_url'] = $langAssetBundle->baseUrl . "/{$langFile}";
+        }
+        // @codeCoverageIgnoreEnd
+
+        $options = Json::encode($this->clientOptions);
+
+        $js[] = "tinymce.init($options);";
+        if ($this->triggerSaveOnBeforeValidateForm) {
+            $js[] = "$('#{$id}').parents('form').on('beforeValidate', function() { tinymce.triggerSave(); });";
+        }
+        $view->registerJs(implode("\n", $js));
+    }
+}
